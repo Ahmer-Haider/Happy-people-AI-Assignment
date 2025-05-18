df9f832e2e83d5adb8083b238a40a0373bb5acbe
Ahmer Haider <ahmerhaider1610@gmail.com>
2025-05-18 22:09:16 +0530

    add frontend folder

---
+++ frontend/JS/Basics/JsBasics05.js
@@ -0,0 +1,10 @@
+const person = {
+  firstName: "rahul",
+  lastName: "koppula",
+};
+console.log(person.firstName);
+console.log(person.lastName);
+
+
+console.log(person["firstName"]);
+console.log(person["lastName"]);
